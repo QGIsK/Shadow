4afb2ffb0106052bc5721eeed1138a52178e1ef9
Demian <devaccdemiann@gmail.com>
2021-01-17 00:43:46 +0100

    Add update prefix method

--- src/utils/Guild.js
+++ src/utils/Guild.js
@@ -2,4 +2,6 @@ module.exports = Client => {
   Client.getGuild = id => Client.DB.Guild.findOne({ guildID: id });
   Client.makeGuild = id => new Client.DB.Guild({ guildID: id }).save();
   Client.deleteGuild = id => Client.DB.Guild.findOneAndDelete({ guildID, id });
+  Client.updatePrefix = (prefix, id) =>
+    Client.DB.Guild.findOneAndUpdate({ guildID: id }, { settings: { prefix } });
 };
