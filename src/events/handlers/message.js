d80d26ba18ced470aedbe686532b7e00de84e8e0
Demian <devaccdemiann@gmail.com>
2021-01-16 22:19:45 +0100

    Basic message handler

---
+++ src/events/handlers/message.js
@@ -0,0 +1,68 @@
+const Discord = require("discord.js");
+
+module.exports = async (Client, message) => {
+  const guildSettings = await Client.getGuild(message.guild.id);
+  if (!guildSettings) Client.makeGuild(message.guild.id);
+
+  const PREFIX = guildSettings
+    ? guildSettings.settings.prefix
+    : Client.defaultPrefix;
+
+  if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  const command =
+    Client.commands.get(commandName) ||
+    Client.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
+
+  if (!command) return;
+
+  if (command.guildOnly && message.channel.type === "dm") {
+    return message.reply("I can't execute that command inside DMs!");
+  }
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${PREFIX}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  if (!Client.cooldowns.has(command.name)) {
+    Client.cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = Client.cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `please wait ${timeLeft.toFixed(
+          1
+        )} more second(s) before reusing the \`${command.name}\` command.`
+      );
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+};
