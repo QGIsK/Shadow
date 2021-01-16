86a6d7d118a6284ea8afdf8153ec51783d5cf0fa
Demian <devaccdemiann@gmail.com>
2021-01-17 00:00:34 +0100

    Fix: save new document

--- src/utils/Guild.js
+++ src/utils/Guild.js
@@ -1,5 +1,5 @@
 module.exports = Client => {
   Client.getGuild = id => Client.DB.Guild.findOne({ guildID: id });
-  Client.makeGuild = id => new Client.DB.Guild({ guildID: id });
+  Client.makeGuild = id => new Client.DB.Guild({ guildID: id }).save();
   Client.deleteGuild = id => Client.DB.Guild.findOneAndDelete({ guildID, id });
 };
