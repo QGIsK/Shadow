d50984b8d8b0eb5e86d426db027c72c141d80981
Demian <devaccdemiann@gmail.com>
2021-01-17 00:53:26 +0100

    Add update blockInvites

--- src/utils/Guild.js
+++ src/utils/Guild.js
@@ -1,7 +1,15 @@
 module.exports = Client => {
+  //Guilds
   Client.getGuild = id => Client.DB.Guild.findOne({ guildID: id });
   Client.makeGuild = id => new Client.DB.Guild({ guildID: id }).save();
   Client.deleteGuild = id => Client.DB.Guild.findOneAndDelete({ guildID, id });
+
+  // Settings
   Client.updatePrefix = (prefix, id) =>
     Client.DB.Guild.findOneAndUpdate({ guildID: id }, { settings: { prefix } });
+
+  Client.blockInvites = id =>
+    Client.DB.Guild.findOneAndUpdate({ GuildID: id }, { settings: { blockInvites: true } });
+  Client.enableInvites = id =>
+    Client.DB.Guild.findOneAndUpdate({ GuildID: id }, { settings: { blockInvites: false } });
 };
