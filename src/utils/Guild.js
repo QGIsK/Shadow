57ea994e67588da54191cd14ac5ea9d836819eba
Demian <devaccdemiann@gmail.com>
2021-01-16 23:46:16 +0100

    Add: DeleteGuild

--- src/utils/Guild.js
+++ src/utils/Guild.js
@@ -1,4 +1,5 @@
 module.exports = Client => {
-  Client.getGuild = id => Client.DB.Guild.findOne({ guildId: id });
+  Client.getGuild = id => Client.DB.Guild.findOne({ guildID: id });
   Client.makeGuild = id => new Client.DB.Guild({ guildID: id });
+  Client.deleteGuild = id => Client.DB.Guild.findOneAndDelete({ guildID, id });
 };
