51f96bf47e01d2265d0258da3e39785560f87881
Demian <devaccdemiann@gmail.com>
2021-01-17 02:36:33 +0100

    Temporarily moved this

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = Client => {
-  Client.on("message", message => require("./handlers/message")(Client, message));
+  Client.on("message", message => require("./src/events/message")(Client, message));
 
   Client.on("disconnect", () => Client.logger.log("Bot is disconnecting...", "warn"))
     .on("ready", () => {
       Client.logger.log("Bot is online", "log");
-      require("../dashboard")(Client);
+      require("./src/dashboard")(Client);
     })
     .on("reconnecting", () => Client.logger.log("Bot reconnecting...", "log"))
     .on("error", e => {
