3e0621d1fd9df8bf45c17fdf04184f705a71d017
Demian <devaccdemiann@gmail.com>
2021-01-17 02:16:23 +0100

    Change: usefindandmodify: false

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,7 @@ mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: true,
+    useFindAndModify: false,
   })
   .then(_ => logger.log("MongoDB connected", "log"))
   .catch(e => {
