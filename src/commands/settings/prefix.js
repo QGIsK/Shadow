3aa082900ea8d2875ceeff18fab275fbb6d6ee7c
Demian <devaccdemiann@gmail.com>
2021-01-17 17:11:28 +0100

    Add alias

--- src/commands/settings/prefix.js
+++ src/commands/settings/prefix.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "prefix",
   description: "Set prefix",
-  aliases: [],
+  aliases: ["setprefix"],
   creatorOnly: false,
   guildOnly: true,
   userPermissions: ["ADMINISTRATOR"],
